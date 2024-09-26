1ee9be997f3e967ca8d7e273a86d9e048e9e96cd
Codelord001 <adawuni2@st.knust.edu.gh>
2024-09-26 21:35:41 +0000

    setup edited

--- Edustream_ai/setup.sh
+++ Edustream_ai/setup.sh
@@ -9,8 +9,10 @@ python -m spacy download en_core_web_sm
 
 # Download and extract Sense2Vec model
 wget https://github.com/explosion/sense2vec/releases/download/v1.0.0/s2v_reddit_2015_md.tar.gz
-tar -xvf s2v_reddit_2015_md.tar.gz
+tar -xvf s2v_reddit_2015_md.tar.gz -C Edustream_ai/
 
 # Move Sense2Vec model to the appropriate directory
-mkdir -p s2v_old
-mv s2v_reddit_2015_md/* s2v_old/
+# Create the s2v_old directory inside Edustream_ai if it doesn't exist
+mkdir -p Edustream_ai/s2v_old
+mv s2v_reddit_2015_md/* Edustream_ai/s2v_old/
+
