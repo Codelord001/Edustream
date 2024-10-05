f75ca513cc305858dce0b15dbb4585e4b0e8e6f0
Codelord001 <adawuni2@st.knust.edu.gh>
2024-10-05 16:37:03 +0000

    setup changes made

--- Edustream_ai/app.py
+++ Edustream_ai/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from Questgen import main
+from .Questgen import main
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
